689d2a3f1ece9248e0eea6ce4fa49174b3dc934b
Hous1423 <kh_talbi@esi.dz>
2024-05-18 14:25:43 +0100

    feat: date consideration and MAIN script

--- MAIN_SCRIPT.py
+++ MAIN_SCRIPT.py
@@ -1,32 +1,35 @@
 import subprocess
-import os
 
-# List of script filenames
 scripts = [
-    "script1.py",
-    "script2.py",
-    "script3.py",
-    # Add more scripts as needed
+    ("newspapers_scraper.py", "scrapy runspider"),
+    ("laws_metadata_scraper.py", "python"),
+    ("9ita3.py", "python"),
+    ("pdfs_to_images_conversion.py", "python"),
+    ("ocr_images.py", "python"),
+    ("text_extraction.py", "python"),
+    ("fix_law_texts.py", "python"),
+    ("delete_all_photos.py", "python")
 ]
 
 def run_scripts(script_list):
-    for script in script_list:
+    for script, command in script_list:
         try:
-            print(f"Executing {script}...")
-            # Execute the script
-            result = subprocess.run(['python', script], check=True)
-            # Check if the script executed successfully
+            print(f"Executing {script} with command '{command}'...")
+            if command == "scrapy runspider":
+                result = subprocess.run(['scrapy', 'runspider', script], check=True)
+            else:
+                result = subprocess.run([command, script], check=True)
+
             if result.returncode == 0:
                 print(f"{script} executed successfully.")
             else:
                 print(f"{script} failed with return code {result.returncode}.")
-                break  # Optional: stop if a script fails
+                break 
         except subprocess.CalledProcessError as e:
             print(f"Execution failed: {e}")
-            break  # Optional: stop if a script fails
+            break 
         except Exception as e:
             print(f"An error occurred: {e}")
-            break  # Optional: stop if a script fails
-
+            break 
 if __name__ == "__main__":
     run_scripts(scripts)
